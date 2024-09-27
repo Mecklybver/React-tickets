ee57ee4f4ae818695f88bc206280fa9f98a46b9c
Mecklybver <mecklybver@gmail.com>
2024-09-27 16:45:32 +0200

    Routes and Controllers added

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-console.log("Server...")
+const express = require("express");
+const color = require("colors");
+const dotenv = require("dotenv").config();
+
+const PORT = process.env.PORT || 5000
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "Welcome to the support desk API"
+    })
+})
+
+//Routes
+app.use("/api/users", require("./routes/userRoutes"))
+
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`.red.bold.underline))
