b4efc00d376783234f5a914dd3637175b3854ea1
Mecklybver <mecklybver@gmail.com>
2024-09-27 17:31:56 +0200

    Connect to database

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,27 @@
-const registerUser = (req, res) => {
-res.send("Register Route");
-}
-const loginUser = (req, res) => {
-res.send("Login Route");
-}
+const asyncHandler = require("express-async-handler");
+
+// @desc Register a new user
+// @route POST /api/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400)
+    throw new Error("Please add all fields");
+  }
+
+  res.send("Register Route");
+});
+
+// @desc Login a user
+// @route POST /api/users/login
+// @access Public
+const loginUser = asyncHandler(async (req, res) => {
+  res.send("Login Route");
+});
 
 module.exports = {
-    registerUser,
-    loginUser
-}
+  registerUser,
+  loginUser,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const color = require("colors");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorHandler");
+const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000
 
+connectDB()
+
 const app = express();
+
+//Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(errorHandler)
 
 app.get("/", (req, res) => {
     res.status(200).json({
@@ -14,5 +23,8 @@ app.get("/", (req, res) => {
 
 //Routes
 app.use("/api/users", require("./routes/userRoutes"))
+
+//Error
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`.red.bold.underline))
